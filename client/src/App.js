6c833b385450e11f84db240364ba5b5f44c593d8
Hayden Nault <naulth@gmail.com>
2023-09-11 12:35:10 -0400

    signuppage ready and create loginpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import {Routes, Route} from "react-router-dom"
 
+import SignupPage from './Components/SignupPage';
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Routes>
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
     </div>
   );
 }
